0e80ab4815c85bfcadc0e8e2c68c21112d8cbfc1
Paksubeen <tnqls520@gmail.com>
2023-02-18 23:00:48 +0900

    💄 Add Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import GlobalStyle from './styles/GlobalStyle';
+
 function App() {
-  return <div>Practice</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>Practice</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import variables from './variables';
+
+const GlobalStyle = createGlobalStyle` 
+  ${variables}
+  ${reset}
+  
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  html, body {
+    font-size: var(--font-size-md);
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
